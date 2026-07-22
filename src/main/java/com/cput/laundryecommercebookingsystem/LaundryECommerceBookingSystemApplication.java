5c9d64447e86a176868882134e063971de825699
Muso Nkuntsu <231223722@mycput.ac.za>
2026-07-23 00:42:41 +0200

    Initial Commit

---
+++ src/main/java/com/cput/laundryecommercebookingsystem/LaundryECommerceBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cput.laundryecommercebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryECommerceBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaundryECommerceBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cput/laundryecommercebookingsystem/LaundryECommerceBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cput.laundryecommercebookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryECommerceBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
